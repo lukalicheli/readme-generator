de5bb62526c7f3d74e046f4479328713def23b66
Luka Licheli <lukalicheli@gmail.com>
2022-01-24 20:17:45 -0800

    copy and pasted from gitlab the required HW contents

---
+++ Develop/index.js
@@ -0,0 +1,9 @@
+// TODO: Include packages needed for this application
+
+// TODO: import the generateMarkdown function from utils/generateMarkdown.js
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
