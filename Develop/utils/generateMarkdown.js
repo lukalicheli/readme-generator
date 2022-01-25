c97f8a32a6a0acb079d97cc2983b9f7417aa389b
Luka Licheli <lukalicheli@gmail.com>
2022-01-24 20:51:56 -0800

    Created readme template inside generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,35 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
+
+Github:
+Email:
+
+
+
 `;
 }
 
